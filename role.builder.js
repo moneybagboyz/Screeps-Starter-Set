ad53ff66996a00100640e2ffb853b32817b2b122
moneybagboyz <72819166+moneybagboyz@users.noreply.github.com>
2022-08-10 15:12:04 -0500

    Create role.builder.js

---
+++ role.builder.js
@@ -0,0 +1,35 @@
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+        }
+        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+            creep.memory.building = true;
+            creep.say('🚧 build');
+        }
+
+        if(creep.memory.building) {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+        else {
+            const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, {
+                filter: resource => resource.resourceType == RESOURCE_ENERGY
+            })
+            const closestDroppedEnergy = creep.pos.findClosestByRange(droppedEnergy)
+            if (creep.pickup(closestDroppedEnergy) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(closestDroppedEnergy, { visualizePathStyle: { stroke: '#ffaa00' } });
+            }
+        }
+    }
+};
+
+module.exports = roleBuilder;
